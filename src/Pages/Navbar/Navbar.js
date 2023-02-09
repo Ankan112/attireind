d3204463d8063b0db496c44b029f4ff3d0af7df3
Ankan112 <ankanhalder112@gmail.com>
2023-02-09 14:53:01 +0600

    design ready

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import logo from '../../images/logo.webp'
+import { BiSearch } from 'react-icons/bi'
+import { FaUserCircle } from 'react-icons/fa'
+import { BsBag } from 'react-icons/bs'
 
 const Navbar = () => {
     return (
         <div className='w-9/12 mx-auto mt-8'>
-            <div className='flex justify-between items-center'>
-                <div>
-                    <span>  </span>
-                </div>
-                <div>
-                    <img className='ml-48' src={logo} alt="" />
-                </div>
-                <div>
-                    <div className='flex'>
-                        <p>Log In</p>
-                        <p>Log In</p>
-                        <p>Log In</p>
+            <div className=''>
+                <div className='flex justify-between items-center '>
+                    <div>
+                        <span>  </span>
                     </div>
                     <div>
-                        <input type="text" />
+                        <img className='ml-48' src={logo} alt="" />
+                    </div>
+                    <div className='mr-7'>
+                        <div style={{ color: '#3E7C7C' }} className='flex items-center justify-end mr-10'>
+                            <p>Log In</p>
+                            <FaUserCircle className='mx-5' size='25'></FaUserCircle>
+                            <BsBag size='25' className=''></BsBag>
+                        </div>
+                        <div className='relative'>
+                            <BiSearch style={{ top: '17px', left: '5px', margin: '7px', color: '#3E7C7C' }} className='absolute' size='20'></BiSearch>
+                            <input style={{ borderColor: '#3E7C7C', color: '#3E7C7C' }} placeholder="Search..." type="text" className='border-2 my-3 py-2 px-6 pl-10' name="" id="" />
+                        </div>
                     </div>
                 </div>
+                <div style={{ color: '#3E7C7C' }} className='flex justify-center'>
+                    <span className='mx-10'>Home</span>
+                    <span className='mx-10 '><span className='w-20 border-b-2 border-indigo-800'>New In</span></span>
+                    <span className='mx-10'>Shop</span>
+                    <span className='mx-10'>About Us</span>
+                </div>
             </div>
-            <div style={{ color: '#3E7C7C' }} className='flex justify-center'>
-                <span className='mx-10'>Home</span>
-                <span className='mx-10 '><span className='w-20 border-b-2 border-indigo-800'>New In</span></span>
-                <span className='mx-10'>Shop</span>
-                <span className='mx-10'>About Us</span>
-            </div>
+
+
         </div>
     );
 };
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,46 +1,79 @@
 import React from 'react';
 import p1 from '../../images/p1.webp'
+import p2 from '../../images/p2.webp'
+import p3 from '../../images/p3.webp'
+import p4 from '../../images/p4.webp'
+import p5 from '../../images/p5.webp'
+import p6 from '../../images/p6.webp'
+import p7 from '../../images/p7.webp'
 
 const Products = () => {
+    const products = [
+        {
+            id: 1,
+            productName: 'Aline Dress',
+            image: p1,
+            currentPrice: 850,
+            regularPrice: 1000
+        },
+        {
+            id: 2,
+            productName: 'Designer Neck Dress',
+            image: p2,
+            currentPrice: 680,
+            regularPrice: 800
+        },
+        {
+            id: 3,
+            productName: 'Sequence Skirt & Shirt Set',
+            image: p3,
+            currentPrice: 1360,
+            regularPrice: 1600
+        },
+        {
+            id: 4,
+            productName: 'Black organza skirt',
+            image: p4,
+            currentPrice: 680,
+            regularPrice: 800
+        },
+        {
+            id: 5,
+            productName: 'Purple zig zag puff sleeve tiered dress',
+            image: p5,
+            currentPrice: 680,
+            regularPrice: 800
+        },
+        {
+            id: 6,
+            productName: 'Sleeveless Floral Maxi Dress',
+            image: p6,
+            currentPrice: 664.15,
+            regularPrice: 899
+        },
+        {
+            id: 7,
+            productName: 'Brown Maxi Dress',
+            image: p7,
+            currentPrice: 664.15,
+            regularPrice: 899
+        },
+    ]
     return (
         <div className='w-9/12 mx-auto mt-10'>
             <div className='grid grid-cols-4'>
-                <div className='text-left relative'>
-                    <p style={{ backgroundColor: '#2A7C6F', padding: '3px 14px' }} className='absolute text-white'>NEW IN</p>
-                    <div>
-                        <img src={p1} alt="" />
-                    </div>
-                    <h2 style={{ color: '#2A7C6F' }} className='mt-3'>Aline Dress</h2>
-                    <span style={{ color: '#40BCA8' }}><strike>$1000</strike></span>
-                    <span style={{ color: '#40BCA8' }} className='ml-2'>$800</span>
-                </div>
-                <div className='text-left relative'>
-                    <p style={{ backgroundColor: '#2A7C6F', padding: '3px 14px' }} className='absolute text-white'>NEW IN</p>
-                    <div>
-                        <img src={p1} alt="" />
-                    </div>
-                    <h2 style={{ color: '#2A7C6F' }} className='mt-3'>Aline Dress</h2>
-                    <span style={{ color: '#40BCA8' }}><strike>$1000</strike></span>
-                    <span style={{ color: '#40BCA8' }} className='ml-2'>$800</span>
-                </div>
-                <div className='text-left relative'>
-                    <p style={{ backgroundColor: '#2A7C6F', padding: '3px 14px' }} className='absolute text-white'>NEW IN</p>
-                    <div>
-                        <img src={p1} alt="" />
-                    </div>
-                    <h2 style={{ color: '#2A7C6F' }} className='mt-3'>Aline Dress</h2>
-                    <span style={{ color: '#40BCA8' }}><strike>$1000</strike></span>
-                    <span style={{ color: '#40BCA8' }} className='ml-2'>$800</span>
-                </div>
-                <div className='text-left relative'>
-                    <p style={{ backgroundColor: '#2A7C6F', padding: '3px 14px' }} className='absolute text-white'>NEW IN</p>
-                    <div>
-                        <img src={p1} alt="" />
-                    </div>
-                    <h2 style={{ color: '#2A7C6F' }} className='mt-3'>Aline Dress</h2>
-                    <span style={{ color: '#40BCA8' }}><strike>$1000</strike></span>
-                    <span style={{ color: '#40BCA8' }} className='ml-2'>$800</span>
-                </div>
+                {
+                    products.map(product => <div key={product.id} className='text-left mb-5 relative'>
+                        <p style={{ backgroundColor: '#2A7C6F', padding: '3px 14px' }} className='absolute text-white'>NEW IN</p>
+                        <div>
+                            <img src={product.image} alt="" />
+                        </div>
+                        <h2 style={{ color: '#2A7C6F' }} className='mt-3'>{product.productName}</h2>
+                        <span style={{ color: '#40BCA8' }}><strike>${product.regularPrice}</strike></span>
+                        <span style={{ color: '#40BCA8' }} className='ml-2'>${product.currentPrice}</span>
+                    </div>)
+                }
+
             </div>
         </div>
     );
